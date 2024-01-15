cbfdb14788495eb8a42b0d13a67748954486497f
manish <senmanish171@gmail.com>
2024-01-16 01:25:34 +0530

    add owl carousel

--- src/components/CoursesCarousel/CoursesCarousel.tsx
+++ src/components/CoursesCarousel/CoursesCarousel.tsx
@@ -1,28 +1,60 @@
 
-//import OwlCarousel from 'react-owl-carousel';
+import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 const CoursesCarousel = () => {
-  const item = {
-    items : [
-       <img src={"case-detail.jpg"} style={{height: "100vh", width: "98vw"}} />,
-       <img src={"case-detail.jpg"} style={{height: "100vh", width: "98vw"}} />,
-       <img src={"case-detail.jpg"} style={{height: "100vh", width: "98vw"}} />
-    ]
-  }
   const options = {
     items:1,
+    loop: true,
+    autoplayTimeout:3000,
+    animateOut: "fadeOut",
+    autoplay:true,
     nav:true,
-    rewind:true,
-    autoplay:true 
-  }
+    dots:true,
+    margin: 10,
+    responsive:{
+      1100:{
+        items:3,
+      },
+      724:{
+        items:1
+      },
+      500:{
+        items:1
+      },
+      370:{
+        items:1,
+        innerWidth:"100%",
+        outerWidth:"100%"
 
+      }
+    }
+  } 
   return(
-    <div className='container'>
-     {item.items}
+  <div className='container'>
+     <h1 className='heading'>Our Courses</h1>
+    <OwlCarousel className='owl-theme' {...options} >
+    <div className='item' >
+         <img src={"blog-01-min.jpg"} style={{height:"200px"}} /> 
+         <p>Certified Life Coaching</p>
     </div>
-  )
+    <div className='item' >
+         <img src={"case-detail.jpg"} style={{height:"200px"}} /> 
+         <p >Advance Diploma in  NPL & Coaching</p>
+    </div>
+    <div className='item' >
+       <img src={"case-studies-2-min-1.jpg"} style={{height:"200px"}} /> 
+       <p >Certified NLP Practitioner & Coach</p>
+    </div>
+    <div className='item' >
+        <img src={"case-studies-4-min.jpg"} style={{height:"200px"}} /> 
+        <p>Certified NLP Master Practitioner</p>
+    </div>
+    
+</OwlCarousel>;
+  </div>
+  ) 
 } 
 
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -31,7 +31,7 @@ const Home = () => {
       </div>
       <AboutNlp/>
       <Counter />
-      <CoursesCarousel/>
+     <CoursesCarousel/>
     </div>
   );
 };
